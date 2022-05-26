a163ee2aea946dc6402aac0bed3d48239e9e64b5
bumsam.pyeon <bumsam.pyeon@quryon.com>
2022-05-26 23:03:09 +0900

    편범삼 올바른괄호

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,22 @@
-public class Solution
-{
-    public int solution()
-    {
-        return 0;
+public class Solution {
+    boolean solution(String s) {
+        final char[] brackets = s.toCharArray();
+        int leftBacket = 0;
+        for (char bracket : brackets) {
+            if(bracket == '('){
+                ++leftBacket;
+            }else{
+                --leftBacket;
+                if(leftBacket < 0){
+                    return false;
+                }
+            }
+        }
+        return leftBacket == 0;
+    }
+
+    public static void main(String[] args) {
+        final Solution solution = new Solution();
+        System.out.println("solution = " + solution.solution("(())()"));
     }
 }
