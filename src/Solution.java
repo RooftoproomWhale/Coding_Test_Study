bf73d32cfcd676f27b14b01be6245c7b178b19ad
bumsam.pyeon <bumsam.pyeon@quryon.com>
2022-05-26 23:54:28 +0900

    편범삼 제로

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,28 @@
-public class Solution
-{
-    public int solution()
-    {
-        return 0;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Solution {
+    public int solution(int k, int[] inputs) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>(k);
+        for (int input : inputs) {
+            if(input != 0){
+                stack.push(input);
+            }else{
+                stack.pop();
+            }
+        }
+        return stack.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("입력 횟수(k)를 입력하시오 : ");
+        int k = Integer.parseInt(scanner.nextLine());
+        int[] inputs = IntStream.range(0, k).map(i ->{
+            System.out.printf("%d번째 정수를 입력하시오 : ",i+1);
+            return Integer.parseInt(scanner.nextLine());
+        }).toArray();
+        System.out.printf("결과 : %d", new Solution().solution(k, inputs));
     }
 }
