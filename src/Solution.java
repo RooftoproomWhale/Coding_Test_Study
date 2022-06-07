cdf0b9355fe2885889840f56adf65ee9acfb9157
RooftoproomWhale <gh8238@naver.com>
2022-06-07 17:11:19 +0900

    신건웅 다리를지나는트럭

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,162 @@
-public class Solution
-{
-    public int solution()
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    class Truck
     {
-        return 0;
+        private int remainDistance;
+        private int weight;
+
+        public Truck(int remainDistance, int weight)
+        {
+            this.remainDistance = remainDistance;
+            this.weight = weight;
+        }
+
+        public boolean isFinished()
+        {
+            return remainDistance == 0;
+        }
+
+        public void move()
+        {
+            remainDistance--;
+        }
+        public int getWeight()
+        {
+            return weight;
+        }
+    }
+    class Bridge
+    {
+        private int length;
+        private final int maxWeight;
+        private final List<Truck> trucks = new ArrayList<>();
+        private int currentWeight = 0;
+
+        public Bridge(int length, int maxWeight)
+        {
+            this.length = length;
+            this.maxWeight = maxWeight;
+        }
+
+        public List<Truck> getTrucks()
+        {
+            return trucks;
+        }
+
+        public boolean isTruckWeightAvailable(Truck truck)
+        {
+            return currentWeight + truck.getWeight() <= maxWeight;
+        }
+
+        public void enterTruck(Truck truck)
+        {
+            currentWeight += truck.getWeight();
+            trucks.add(truck);
+            truck.move();
+        }
+
+        public void moveTrucks()
+        {
+            for (Truck truck : trucks) {
+                truck.move();
+            }
+        }
+
+        public int getLength()
+        {
+            return length;
+        }
+
+        public void setLength(int length)
+        {
+            this.length = length;
+        }
+
+        public void removeIfArrived()
+        {
+            if (trucks.get(0).isFinished())
+            {
+                currentWeight -= trucks.get(0).getWeight();
+                trucks.remove(0);
+            }
+        }
+    }
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        /*
+        ArrayList<Integer> completeTruck = new ArrayList<>();
+        ArrayDeque<Integer> bridgeQueue = new ArrayDeque<>();
+
+
+        for(int i = 0 ; i<bridge_length ; i++)
+        {
+            bridgeQueue.offer(0);
+        }
+
+        int currentWeight = 0;
+        int index = 0;
+        while(completeTruck.size() != truck_weights.length)
+        {
+            currentWeight-=bridgeQueue.peek();
+            if(bridgeQueue.peek() != 0)
+                completeTruck.add(bridgeQueue.peek());
+            bridgeQueue.poll();
+
+            if(index < truck_weights.length)
+            {
+                if(currentWeight + truck_weights[index] <= weight)
+                {
+                    bridgeQueue.offer(truck_weights[index]);
+                    currentWeight += truck_weights[index];
+                    index++;
+                }
+                else
+                {
+                    bridgeQueue.offer(0);
+                }
+            }
+            else
+            {
+                answer+=bridge_length;
+                break;
+            }
+
+            answer++;
+        }
+        */
+
+        Bridge bridge = new Bridge(bridge_length, weight);
+        Queue<Truck> waiting_trucks = new LinkedList<>();
+        for (int w : truck_weights)
+        {
+            waiting_trucks.offer(new Truck(bridge_length, w));
+        }
+
+        while (!waiting_trucks.isEmpty() || bridge.getTrucks().size() > 0)
+        {
+            Truck truck = waiting_trucks.peek();
+            if (truck == null)
+            {
+                bridge.removeIfArrived();
+                bridge.moveTrucks();
+                answer++;
+                continue;
+            }
+            bridge.moveTrucks();
+            if (bridge.isTruckWeightAvailable(truck))
+            {
+                waiting_trucks.poll();
+                bridge.enterTruck(truck);
+            }
+            bridge.removeIfArrived();
+            answer++;
+        }
+
+        return answer;
     }
 }
---
+++ src/SolutionTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solution()
+    {
+        assertEquals(8,new Solution().solution(2,10,new int[]{7,4,5,6}));
+        assertEquals(101,new Solution().solution(100,100,new int[]{10}));
+        assertEquals(110,new Solution().solution(100,100,new int[]{10,10,10,10,10,10,10,10,10,10}));
+    }
+}
