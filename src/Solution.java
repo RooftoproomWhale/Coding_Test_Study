1337d985b4d90d2388471862b4a2bcb7acde1baa
RooftoproomWhale <gh8238@naver.com>
2022-06-30 17:07:33 +0900

    신건웅 길 찾기 게임

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
 public class Solution
 {
-    public int solution()
+
+    public int[] solution(int[] array, int[][] commands)
     {
-        return 0;
+        int[] answer = new int[commands.length];
+        int index = 0;
+        for(int[] command : commands)
+        {
+            int i = command[0];
+            int j = command[1];
+            int k = command[2];
+
+            int[] cutArray= Arrays.copyOfRange(array,i - 1,j);
+            Arrays.sort(cutArray);
+            answer[index] = cutArray[k - 1];
+            index++;
+        }
+
+        return answer;
     }
 }
